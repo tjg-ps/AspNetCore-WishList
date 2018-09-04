ec2351773929709723cf3601e01701c3dc777c92
tjg-ps <paul.seagrave@jobsite.co.uk>
2018-09-04 15:07:09 +0100

    finished the module

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var model = _context.Items.ToList();
+            return View("Index", model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Models.Item item)
+        {
+            _context.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            _context.Remove(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
