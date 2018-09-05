df4675a9c37f8acfd2c6cb37dd6ba5702e901693
tjg-ps <paul.seagrave@jobsite.co.uk>
2018-09-05 14:34:19 +0100

    finished

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -11,7 +11,7 @@ namespace WishList.Models
         public int Id { get; set; }
         [Required]
         [MaxLength(50)]
-        public String Description { get; set; }
+        public string Description { get; set; }
 
     }
 }
